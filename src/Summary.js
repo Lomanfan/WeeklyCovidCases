20c439c57ad27e3bc1f36d9f20bd9ba1a098af97
Lomanfan <lfan.canada@gmail.com>
2022-01-02 18:36:38 -0700

    add arrow icons, update styles

--- src/Summary.js
+++ src/Summary.js
@@ -1,15 +1,38 @@
 import React from "react";
-import Row from "./Row";
+import { BsArrowUpShort, BsArrowDownShort } from "react-icons/bs";
 import numberWithCommas from "./Common/numberWithCommas";
+import "./styles.css";
 
 const Summary = ({ data }) => {
+  const changeInCases = ({ data }) => {
+    const numOfChanges = data.active_cases_change;
+    if (numOfChanges === 0) return <div>[0]</div>;
+    if (numOfChanges > 0)
+      return (
+        <div>
+          [{numberWithCommas(numOfChanges)}
+          <BsArrowUpShort style={{ display: "inline" }} />]
+        </div>
+      );
+    if (numOfChanges < 0)
+      return (
+        <div>
+          [{numberWithCommas(numOfChanges)}
+          <BsArrowDownShort style={{ display: "inline" }} />]
+        </div>
+      );
+  };
+
   return (
     data && (
       <div className="md-2">
         <h2 className="text-lg">Last updated on: {data.date}</h2>
-        <Row label="Current Cases">{`${numberWithCommas(
-          data.active_cases
-        )} (${numberWithCommas(data.active_cases_change)})`}</Row>
+        <div className="summary">
+          <div>
+            Current Total Active Cases: {numberWithCommas(data.active_cases)}
+          </div>
+          <div>{changeInCases({ data })}</div>
+        </div>
       </div>
     )
   );
